8582ac27d249c1a72e9508571d9cc08456989e29
Shaun Radgowski <67354145+sradgowski@users.noreply.github.com>
2020-08-13 01:46:17 -0400

    Update cofiCostFunc.m

--- cofiCostFunc.m
+++ cofiCostFunc.m
@@ -1,6 +1,6 @@
 function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
                                   num_features, lambda)
-%COFICOSTFUNC Collaborative filtering cost function
+% COFICOSTFUNC Collaborative filtering cost function
 %   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
 %   num_features, lambda) returns the cost and gradient for the
 %   collaborative filtering problem.
@@ -12,32 +12,23 @@ Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
             
-% You need to return the following values correctly
+% Initialize
 J = 0;
 X_grad = zeros(size(X));
 Theta_grad = zeros(size(Theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost function and gradient for collaborative
-%               filtering. Concretely, you should first implement the cost
-%               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
-%               gradient and use the checkCostFunction routine to check
-%               that the gradient is correct. Finally, you should implement
-%               regularization.
+% ====================== NOTES ======================
 %
-% Notes: X - num_movies  x num_features matrix of movie features
-%        Theta - num_users  x num_features matrix of user features
-%        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
-%            i-th movie was rated by the j-th user
+%      X - num_movies  x num_features matrix of movie features
+%      Theta - num_users  x num_features matrix of user features
+%      Y - num_movies x num_users matrix of user ratings of movies
+%      R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+%          i-th movie was rated by the j-th user
 %
-% You should set the following variables correctly:
-%
-%        X_grad - num_movies x num_features matrix, containing the 
-%                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
-%                     partial derivatives w.r.t. to each element of Theta
+%      X_grad - num_movies x num_features matrix, containing the 
+%               partial derivatives w.r.t. to each element of X
+%      Theta_grad - num_users x num_features matrix, containing the 
+%                   partial derivatives w.r.t. to each element of Theta
 %
 
 J = (1/2)*sum(sum(((R.*(X*Theta')) - (R.*Y)).^2));
@@ -53,8 +44,6 @@ Theta_grad_reg = lambda .* Theta;
 
 X_grad = X_grad + X_grad_reg
 Theta_grad = Theta_grad + Theta_grad_reg
-
-% =============================================================
 
 grad = [X_grad(:); Theta_grad(:)];
 
